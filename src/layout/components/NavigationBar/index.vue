5d81c13fa81a1d866a6256640dd5fb068e894f8c
zuochuangshang <1105069709@qq.com>
2025-02-11 19:00:17 +0800

    add gh-pages

--- src/layout/components/NavigationBar/index.vue
+++ src/layout/components/NavigationBar/index.vue
@@ -51,12 +51,12 @@ function logout() {
         </div>
         <template #dropdown>
           <el-dropdown-menu>
-            <a target="_blank" href="">
+            <a target="_blank" href="https://github.com/xiaowei123h/vue-base-reference.git">
               <el-dropdown-item>GitHub</el-dropdown-item>
             </a>
-            <a target="_blank" href="">
+            <!-- <a target="_blank" href="">
               <el-dropdown-item>Gitee</el-dropdown-item>
-            </a>
+            </a> -->
             <el-dropdown-item divided @click="logout">
               退出登录
             </el-dropdown-item>
